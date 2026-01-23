dee0a10ce05ec1f345901aef77ee6c6bd70611ba
kjh <145811+kjh@users.noreply.github.com>
2026-01-23 22:50:15 +0200

    uibuild

--- osa3/puhelinluettelobackend/models/person.js
+++ osa3/puhelinluettelobackend/models/person.js
@@ -17,10 +17,20 @@ mongoose.connect(url, { family: 4 })
 const personSchema = new mongoose.Schema({
   name: {
     type: String,
-    minlength: 5,
+    minlength: 3,
     //required: true
   },
-  number: String,
+  number: {
+    type: String,
+    minlength: 8,
+    //required: true
+    validate: [{
+      validator: function(v) {
+        return /^\d{2,3}-\d{7}$/.test(v);
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    }],
+  },
 })
 
 personSchema.set('toJSON', {
